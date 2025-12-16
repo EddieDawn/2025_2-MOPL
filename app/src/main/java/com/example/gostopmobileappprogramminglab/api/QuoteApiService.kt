fc186836f3bbce04045e6d1b68786344c7030ba4
EddieDawn <autumnchristmas0613@gmail.com>
2025-12-16 20:00:27 +0900

    1

--- app/src/main/java/com/example/gostopmobileappprogramminglab/api/QuoteApiService.kt
+++
@@ -1,8 +0,0 @@
-package com.example.gostopmobileappprogramminglab.api
-
-import retrofit2.http.GET
-
-interface QuoteApiService {
-    @GET("api/random")
-    suspend fun getRandomQuote(): List<ZenQuoteResponse>
-}
